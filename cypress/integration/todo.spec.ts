f8af391f91078e1c449cef599158fc1dafcb0fd8
Matias Berrutti <berrutti@gmail.com>
2021-10-11 21:23:22 -0300

    Add Cypress tests scaffolding

---
+++ cypress/integration/todo.spec.ts
@@ -0,0 +1,13 @@
+describe("todo app", () => {
+  it("can add new tasks", () => {});
+
+  it("shows the creation time of the task", () => {});
+
+  it("shows the edition time of the task when the task is edited", () => {});
+
+  it("can check off task as completed", () => {});
+
+  it("filters tasks by status", () => {});
+
+  it("removes tasks when the delete button is clicked");
+});
