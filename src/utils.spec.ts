af76de98e7e1da3362527ab19194d0ddae45936c
Matias Berrutti <berrutti@gmail.com>
2021-10-11 21:39:05 -0300

    Add unit tests

---
+++ src/utils.spec.ts
@@ -0,0 +1,34 @@
+import { FilterValues } from "./components/FilterSelector";
+import { ITask } from "./components/Task";
+import { byFilterValue } from "./utils";
+
+describe("byFilterValue", () => {
+  const todoTask: ITask = {
+    id: "123",
+    text: "Take out the trash",
+    createdTime: "2021-10-12T00:30:32.763Z",
+    done: false,
+  };
+
+  const doneTask: ITask = {
+    id: "456",
+    text: "Write some tests",
+    createdTime: "2021-10-11T00:30:32.763Z",
+    done: true,
+  };
+
+  it('does not filter the task when the selected filter is "all"', () => {
+    expect(byFilterValue(todoTask, FilterValues.ALL)).toEqual(true);
+    expect(byFilterValue(doneTask, FilterValues.ALL)).toEqual(true);
+  });
+
+  it('filters the undone task when the selected filter is "done"', () => {
+    expect(byFilterValue(todoTask, FilterValues.DONE)).toEqual(false);
+    expect(byFilterValue(doneTask, FilterValues.DONE)).toEqual(true);
+  });
+
+  it('filters the done task when the selected filter is "undone"', () => {
+    expect(byFilterValue(doneTask, FilterValues.UNDONE)).toEqual(false);
+    expect(byFilterValue(todoTask, FilterValues.UNDONE)).toEqual(true);
+  });
+});
